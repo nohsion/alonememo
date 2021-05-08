c14ef19880a274c8fe718a884881143220e17bf3
Sion Noh <shtldhs123@ajou.ac.kr>
2021-05-08 13:37:33 +0900

    index.html Rendering

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # flask Web Server 생성하기
 app = Flask(__name__)
 
-# app.py 파일을 직접 실행시킬 때 동작시킴
+
+# API 추가
+@app.route('/', methods=['GET'])  # 데코레이터 문법 @
+def index():
+    return render_template('index.html', test='테스트')
+
+
+# app.py 파일을 직접 실행시킬 때 동작시킴. 이 코드가 가장 아랫부분이어야 함!
 if __name__ == '__main__':
     app.run(
         '0.0.0.0',  # 모든 IP 에서 오는 요청을 허용
